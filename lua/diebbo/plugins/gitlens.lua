badc9bf5e31662b6ed69a4e87f045b0b3dd3d0af
Diebbo <diego.barbieri03@libero.it>
2024-06-19 17:07:13 +0200

    adding landing page

--- lua/diebbo/plugins/gitlens.lua
+++ lua/diebbo/plugins/gitlens.lua
@@ -3,9 +3,10 @@ return {
   config = function ()
     vim.g.blamer_enabled = 1
     vim.g.blamer_delay = 500
+    vim.g.blamer_prefix = ''
     vim.g.blamer_show_in_visual_modes = 0
-    vim.g.blamer_show_in_insert_modes = 0
+    vim.g.blamer_show_in_insert_modes = 1
     vim.g.blamer_show_in_replace_modes = 0
-    vim.g.blamer_show_in_normal_modes = 1
+    vim.g.blamer_show_in_normal_modes = 0
   end
 }
