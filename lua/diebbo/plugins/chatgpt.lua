50c3cec0e589b6ca7238fb1cf3e5ce113b60d37d
Diebbo <diego.barbieri03@libero.it>
2024-10-17 12:22:18 +0200

    chatgpt init

--- lua/diebbo/plugins/chatgpt.lua
+++ lua/diebbo/plugins/chatgpt.lua
@@ -1,58 +1,72 @@
 return {
-    "jackMort/ChatGPT.nvim",
-    event = "VeryLazy",
+    'jackMort/ChatGPT.nvim',
+    event = 'VeryLazy',
     dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "folke/trouble.nvim",
-      "nvim-telescope/telescope.nvim"
+        'MunifTanjim/nui.nvim',
+        'nvim-lua/plenary.nvim',
+        'folke/trouble.nvim',
+        'nvim-telescope/telescope.nvim',
     },
     config = function()
+        local function is_env_file_missing()
+            local home = os.getenv 'HOME'
+            local env_file = home .. '/.config/nvim/.env'
+            local file = io.open(env_file, 'r')
+            if file then
+                file:close()
+                return false
+            else
+                return true
+            end
+        end
+
         local function get_api_key()
-            local handle = io.popen("pass show api/chatgpt/api_key")
+            local handle = io.popen 'pass show api/chatgpt/api_key'
             if handle then
-                local api_key = handle:read("*a"):gsub("[\n\r]", "")  -- Remove newline characters
+                local api_key = handle:read('*a'):gsub('[\n\r]', '') -- Remove newline characters
                 handle:close()
                 return api_key
             else
-                error("Failed to run 'pass' command")
+                error "Failed to run 'pass' command"
             end
         end
 
         local function write_to_env_file(key, value)
-            local home = os.getenv("HOME")
-            local env_file = home .. "/.config/nvim/.env"
-            local file = io.open(env_file, "r")
-            local contents = ""
+            local home = os.getenv 'HOME'
+            local env_file = home .. '/.config/nvim/.env'
+            local file = io.open(env_file, 'r')
+            local contents = ''
             local key_exists = false
 
             if file then
-                contents = file:read("*all")
+                contents = file:read '*all'
                 file:close()
-                for line in contents:gmatch("[^\r\n]+") do
-                    if line:match("^" .. key .. "=") then
+                for line in contents:gmatch '[^\r\n]+' do
+                    if line:match('^' .. key .. '=') then
                         key_exists = true
                         break
                     end
                 end
             end
 
-            file = io.open(env_file, "a+")
+            file = io.open(env_file, 'a+')
             if file then
                 if not key_exists then
-                    file:write(key .. "=" .. value .. "\n")
+                    file:write(key .. '=' .. value .. '\n')
                 end
                 file:close()
             else
-                error("Failed to open .env file for writing")
+                error 'Failed to open .env file for writing'
             end
         end
 
-        local api_key = get_api_key()
-        write_to_env_file("OPENAI_API_KEY", api_key)
+        if is_env_file_missing() then
+            local api_key = get_api_key()
+            write_to_env_file('OPENAI_API_KEY', api_key)
+        end
 
-        require("chatgpt").setup({
-            api_key_cmd = "echo " .. vim.fn.shellescape(api_key),
-        })
+        require('chatgpt').setup {
+            api_key_cmd = 'echo ' .. vim.fn.shellescape(api_key),
+        }
     end,
 }
