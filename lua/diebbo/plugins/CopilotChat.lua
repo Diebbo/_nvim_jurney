fae426f235f2a09b99deb2ce2b261805ec3c9688
Diebbo <diego.barbieri03@libero.it>
2024-10-30 22:44:37 +0100

    copilot

---
+++ lua/diebbo/plugins/CopilotChat.lua
@@ -0,0 +1,21 @@
+return {
+  'CopilotC-Nvim/CopilotChat.nvim',
+  branch = 'canary',
+  dependencies = {
+    { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
+    { 'nvim-lua/plenary.nvim' },  -- for curl, log wrapper
+  },
+  build = 'make tiktoken',        -- Only on MacOS or Linux
+  opts = {
+    debug = true,                 -- Enable debugging
+    window = {
+      layout = 'vertical',
+      width = 50,
+    },
+    mappings = {
+      close = {
+        insert = '',
+      },
+    },
+  },
+}
