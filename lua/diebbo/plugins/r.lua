68fc74e40131d1dfbf9906a2d7020b56b48ea0c4
Diebbo <diego.barbieri03@libero.it>
2024-11-25 10:53:10 +0100

    zio pera era colpa di un env

--- lua/diebbo/plugins/r.lua
+++ lua/diebbo/plugins/r.lua
@@ -2,6 +2,10 @@ return {
   'R-nvim/R.nvim',
   -- Only required if you also set defaults.lazy = true
   lazy = false,
+  -- R.nvim is still young and we may make some breaking changes from time
+  -- to time. For now we recommend pinning to the latest minor version
+  -- like so:
+  version = '~0.1.0',
   config = function()
     -- Create a table with the options to be passed to setup()
     local opts = {
@@ -14,8 +18,8 @@ return {
       R_args = { '--quiet', '--no-save' },
       min_editor_width = 72,
       rconsole_width = 78,
-      objbr_mappings = {                                -- Object browser keymap
-        c = 'class',                                    -- Call R functions
+      objbr_mappings = { -- Object browser keymap
+        c = 'class', -- Call R functions
         ['<localleader>gg'] = 'head({object}, n = 15)', -- Use {object} notation to write arbitrary R code.
         v = function()
           -- Run lua functions
@@ -35,13 +39,8 @@ return {
     if vim.env.R_AUTO_START == 'true' then
       opts.auto_start = 'on startup'
       opts.objbr_auto_start = true
+      vim.notify('R.nvim: Auto-starting R', 'info', { title = 'R.nvim' })
     end
     require('r').setup(opts)
-    vim.api.nvim_create_autocmd('BufWritePre', {
-      pattern = '*.R',
-      callback = function()
-        vim.lsp.buf.format { async = false }
-      end,
-    })
   end,
 }
