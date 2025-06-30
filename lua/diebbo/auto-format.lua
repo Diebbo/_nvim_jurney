d9dfa358fd9a66d9b34d84190f7cd3fc0b8a8587
Diebbo <diego.barbieri03@libero.it>
2025-06-30 12:28:03 +0200

    autoforamt

---
+++ lua/diebbo/auto-format.lua
@@ -0,0 +1,33 @@
+do
+  local id = 0
+  function toggle_autoformat()
+    if id ~= 0 then
+      vim.api.nvim_del_autocmd(id)
+      id = 0
+      vim.notify('Autoformat disabled')
+    else
+      id = vim.api.nvim_create_autocmd(
+        'BufWritePre', {
+          pattern = '*',
+          command = 'lua vim.lsp.buf.format()',
+        })
+      if id ~= 0 then
+        vim.notify('Autoformat enabled')
+      else
+        vim.notify('Failed to enable autoformat')
+      end
+    end
+  end
+
+  function show_format_status()
+    if id ~= 0 then
+      vim.notify('Autoformat enable' .. id)
+    else
+      vim.notify('Autoformat disabled')
+    end
+  end 
+
+  vim.keymap.set('n', '<leader>ff', toggle_autoformat, { desc = 'Toggle [F]ormat' })
+  vim.keymap.set('n', '<leader>fs', show_format_status, { desc = 'Show [F]ormat [S]tatus' })
+
+end
--- lua/diebbo/init.lua
+++ lua/diebbo/init.lua
@@ -2,3 +2,4 @@ require("lazy").setup("diebbo.plugins")
 
 require('diebbo.keymaps')
 require('diebbo.settings')
+require('diebbo.auto-format')
