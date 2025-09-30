92d1e433b744e9259ba3359d9e1636af45e6d57a
Diebbo <diego.barbieri03@libero.it>
2025-09-30 17:44:05 +0200

    coqtail nerdy

---
+++ lua/diebbo/plugins/coqtail.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'whonore/Coqtail',
+    init = function() end,
+    config = function()
+      vim.api.nvim_set_keymap("i", '<A-n>', '<cmd>RocqNext<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", '<A-n>', '<cmd>RocqNext<CR>', { noremap = true, silent = true })
+    end,
+  },
+}
