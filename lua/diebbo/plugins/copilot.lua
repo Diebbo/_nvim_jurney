9c6e5948f66ec57b0feac8005646bafa7d98ae5e
Diebbo <diego.barbieri03@libero.it>
2025-09-30 17:43:57 +0200

    copilot refactor

--- lua/diebbo/plugins/copilot.lua
+++ lua/diebbo/plugins/copilot.lua
@@ -1,5 +1,25 @@
 return {
   {
     'github/copilot.vim',
+    config = function()
+      -- vim.g.copilot_filetypes = {
+      --   ['*'] = false,  -- Disable for all file types by default
+      --   ['javascript'] = true,
+      --   ['typescript'] = true,
+      --   ['python'] = true,
+      --   ['lua'] = true,
+      --   ['html'] = true,
+      --   ['css'] = true,
+      -- }
+      local set_k = vim.api.nvim_set_keymap
+      set_k('i', '<C-g>', 'copilot#Accept("\\<CR>")', { expr = true, silent = true })
+      set_k('i', '<C-l>', '<Plug>(copilot-accept-word)', { noremap = true, silent = true })
+      vim.g.copilot_no_tab_map = true  -- Disable default tab mapping
+    
+      -- activate - deactivate copilot
+      set_k('n', '<leader>ce', ':Copilot enable<CR>', { noremap = true, silent = true })
+      set_k('n', '<leader>cd', ':Copilot disable<CR>', { noremap = true, silent = true })
+
+    end,
   },
 }
