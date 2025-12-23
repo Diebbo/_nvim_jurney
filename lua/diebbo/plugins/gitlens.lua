1fecc8b620cf8a975fd2eb6b92c1b06af44d99ff
Diebbo <die.barbieri03@proton.me>
2025-12-23 16:39:01 +0100

    git refactor

--- lua/diebbo/plugins/gitlens.lua
+++ lua/diebbo/plugins/gitlens.lua
@@ -1,4 +1,6 @@
 return {
+  {
+    enabled = false,
   'APZelos/blamer.nvim',
   config = function ()
     vim.g.blamer_enabled = 1
@@ -9,4 +11,5 @@ return {
     vim.g.blamer_show_in_replace_modes = 0
     vim.g.blamer_show_in_normal_modes = 0
   end
+  },
 }
--- lua/diebbo/plugins/gitsigns.lua
+++ lua/diebbo/plugins/gitsigns.lua
@@ -1,39 +1,87 @@
 return {
-	-- Adds git related signs to the gutter, as well as utilities for managing changes
-	'lewis6991/gitsigns.nvim',
-	opts = {
-		-- See `:help gitsigns.txt`
-		signs = {
-			add = { text = '+' },
-			change = { text = '~' },
-			delete = { text = '_' },
-			topdelete = { text = '‾' },
-			changedelete = { text = '~' },
-		},
-		on_attach = function(bufnr)
-			vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk,
-				{ buffer = bufnr, desc = 'Preview git hunk' })
+  -- Adds git related signs to the gutter, as well as utilities for managing changes
+  'lewis6991/gitsigns.nvim',
+  opts = {
+    -- See `:help gitsigns.txt`
+    signs = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+    },
+    on_attach = function(bufnr)
+      vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
 
-			-- don't override the built-in and fugitive keymaps
-			local gs = package.loaded.gitsigns
-			vim.keymap.set({ 'n', 'v' }, ']c', function()
-				if vim.wo.diff then
-					return ']c'
-				end
-				vim.schedule(function()
-					gs.next_hunk()
-				end)
-				return '<Ignore>'
-			end, { expr = true, buffer = bufnr, desc = 'Jump to next hunk' })
-			vim.keymap.set({ 'n', 'v' }, '[c', function()
-				if vim.wo.diff then
-					return '[c'
-				end
-				vim.schedule(function()
-					gs.prev_hunk()
-				end)
-				return '<Ignore>'
-			end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
-		end,
-	},
+      -- don't override the built-in and fugitive keymaps
+      local gs = package.loaded.gitsigns
+      vim.keymap.set({ 'n', 'v' }, ']c', function()
+        if vim.wo.diff then
+          return ']c'
+        end
+        vim.schedule(function()
+          gs.next_hunk()
+        end)
+        return '<Ignore>'
+      end, { expr = true, buffer = bufnr, desc = 'Jump to next hunk' })
+      vim.keymap.set({ 'n', 'v' }, '[c', function()
+        if vim.wo.diff then
+          return '[c'
+        end
+        vim.schedule(function()
+          gs.prev_hunk()
+        end)
+        return '<Ignore>'
+      end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
+      local gitsigns = require 'gitsigns'
+      local map = function(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        opts.noremap = true
+        opts.silent = true
+        if opts.desc then
+          opts.desc = 'Gitsigns: ' .. opts.desc
+        end
+        vim.keymap.set(mode, l, r, opts)
+      end
+      -- Actions
+      map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'Stage hunk' })
+      map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'Reset hunk' })
+
+      map('v', '<leader>hs', function()
+        gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+      end, {desc = 'Stage hunk'})
+
+      map('v', '<leader>hr', function()
+        gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+      end, {desc = 'Reset hunk'})
+
+      map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'Stage buffer' })
+      map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'Reset buffer' })
+      map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'Preview hunk' })
+      map('n', '<leader>hi', gitsigns.preview_hunk_inline, { desc = 'Preview hunk inline' })
+
+      map('n', '<leader>hb', function()
+        gitsigns.blame_line { full = true }
+      end, { desc = 'Blame line' })
+
+      map('n', '<leader>hd', gitsigns.diffthis, { desc = 'Diff this' })
+
+      map('n', '<leader>hD', function()
+        gitsigns.diffthis '~'
+      end, { desc = 'Diff this against ~' })
+
+      map('n', '<leader>hQ', function()
+        gitsigns.setqflist 'all'
+      end, { desc = 'Set qflist (all)' })
+      map('n', '<leader>hq', gitsigns.setqflist, { desc = 'Set qflist (unstaged)' })
+
+      -- Toggles
+      map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = 'Toggle current line blame' })
+      map('n', '<leader>tw', gitsigns.toggle_word_diff, { desc = 'Toggle word diff' })
+
+      -- Text object
+      map({ 'o', 'x' }, 'ih', gitsigns.select_hunk, { desc = 'Select hunk' })
+    end,
+  },
 }
