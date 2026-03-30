127690558b0c07ddb9f40dc093d4ea66ec9bd567
Diebbo <die.barbieri03@proton.me>
2026-03-30 16:03:21 +0200

    typst update

--- lua/diebbo/plugins/typst.lua
+++ lua/diebbo/plugins/typst.lua
@@ -1,19 +1,29 @@
 return {
-  'chomosuke/typst-preview.nvim',
-  ft = 'typst',
-  version = '1.*',
-  config = function()
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = 'typst',
-      callback = function()
-        vim.opt_local.wrap = true
-        vim.opt_local.spell = true
-        vim.opt_local.shiftwidth = 2
-        vim.keymap.set('n', '<leader>tp', '<cmd>TypstPreview<CR>', {
-          desc = 'Typst Preview',
-          buffer = true,
-        })
-      end,
-    })
-  end,
+  {
+    'chomosuke/typst-preview.nvim',
+    ft = 'typst',
+    version = '1.*',
+    -- opts = {
+    --   open_cmd = 'sioyek %s',
+    -- },
+    enabled = false,
+    config = function()
+      require('typst-preview').setup({
+        open_cmd = 'sioyek --new-window %s',
+      })
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'typst',
+        callback = function()
+          vim.opt_local.wrap = true
+          vim.opt_local.spell = true
+          vim.opt_local.shiftwidth = 2
+          vim.keymap.set('n', '<leader>tp', '<cmd>TypstPreview<CR>', {
+            desc = 'Typst Preview',
+            buffer = true,
+          })
+        end,
+      })
+    end,
+  },
 }
